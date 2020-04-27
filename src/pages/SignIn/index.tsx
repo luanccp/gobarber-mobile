4482e405a24a6ec70f160aa6e1ef77310b3b4bba
Luan Castheloge <luanccp@gmail.com>
2020-04-27 02:53:07 -0300

    style: add icons and signin screen started

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-
 import { Image } from 'react-native';
-import { Container, Title } from './styles';
 
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
@@ -10,6 +12,11 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
