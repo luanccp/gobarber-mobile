798091937004201f1f41f4dafcba9d5d8bcd0571
Luan Castheloge <luanccp@gmail.com>
2020-04-27 01:18:47 -0300

    perf: prettier  eslint editor config

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'no-unused-expressions':'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js', '.tsx', '.ts'] },
+    ],
+    'import/no-cycle': 'off',
+    'import/prefer-default-export': 'off',
+    "no-unused-vars": 'off',
+    'no-use-before-define': ['error', { variables: false }],
+    'func-names': ['error', 'never'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        jsx: 'never',
+        mjs: 'never',
+      },
+    ],
+    'import/no-extraneous-dependencies': ['warn'],
+  },
+};
