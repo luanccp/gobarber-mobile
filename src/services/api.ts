9e63da46da82b37e18e1adda45aa21bb51b0b348
Luan Castheloge <luanccp@gmail.com>
2020-08-10 20:14:45 -0300

    styles: new routes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  // we have to set the same IP as env in API
+  baseURL: 'http://192.168.15.17:3333',
 });
 
 export default api;
