f4a01b46078b7aa92a1e35b348f412651bfa613a
Luan Castheloge <luanccp@gmail.com>
2020-08-30 19:50:09 -0300

    styles: ajust on created appointment screen

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,6 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import {
   Container,
   Title,
@@ -9,8 +11,15 @@ import {
   OKButtonText,
 } from './styles';
 
+interface RouteParams {
+  date: number;
+}
+
 const AppointmentCreated: React.FC = () => {
   const { reset } = useNavigation();
+  const { params } = useRoute();
+
+  const routeParams = params as RouteParams;
 
   const handleOKPressed = useCallback(() => {
     reset({
@@ -23,12 +32,19 @@ const AppointmentCreated: React.FC = () => {
     });
   }, []);
 
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      { locale: ptBR }
+    );
+  }, [routeParams.date]);
   return (
     <Container>
       <Icon name="check" size={80} color="#04d361" />
 
       <Title>Agendamento concluído</Title>
-      <Description>ASJKDHLASD</Description>
+      <Description>{formattedDate}</Description>
 
       <OKButton onPress={handleOKPressed}>
         <OKButtonText>Ok</OKButtonText>
