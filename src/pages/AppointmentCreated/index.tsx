0e241009456ab775c760675eb3b4e1be3f53f308
Luan Castheloge <luanccp@gmail.com>
2020-08-30 18:48:57 -0300

    styles: appointment created screen

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  Title,
+  Description,
+  OKButton,
+  OKButtonText,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOKPressed = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      index: 0,
+    });
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text>AppointmentCreated</Text>
-    </View>
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>ASJKDHLASD</Description>
+
+      <OKButton onPress={handleOKPressed}>
+        <OKButtonText>Ok</OKButtonText>
+      </OKButton>
+    </Container>
   );
 };
 
