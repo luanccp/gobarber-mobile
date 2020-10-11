b2a7dbbb9b06698f3ce8f5d8b9e76df1b233e18e
Luan Castheloge <luanccp@gmail.com>
2020-10-11 11:47:13 +0100

    chore: prepare the enviroment for tests and add for sign in

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     es6: true,
     browser: true,
+    jest: true,
   },
   settings: {
     'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
@@ -33,15 +34,15 @@ module.exports = {
     'prettier/prettier': 'error',
     'react/jsx-props-no-spreading': 'off',
     'react/prop-types': 'off',
-    'no-unused-expressions':'off',
+    'no-unused-expressions': 'off',
     'react/jsx-filename-extension': [
       'warn',
       { extensions: ['.jsx', '.js', '.tsx', '.ts'] },
     ],
     'import/no-cycle': 'off',
     'import/prefer-default-export': 'off',
-    "no-unused-vars": 'off',
-    'camelcase':'off',
+    'no-unused-vars': 'off',
+    camelcase: 'off',
     'no-use-before-define': ['error', { variables: false }],
     'func-names': ['error', 'never'],
     'import/extensions': [
---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-native-testing-library';
+import SignIn from '../../pages/SignIn';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: jest.fn(),
+  };
+});
+
+describe('SignIn Page', () => {
+  it('should contains email/password input', () => {
+    // if use getbyTestId add a 'testID' on the element
+
+    const { getByPlaceholder } = render(<SignIn />);
+
+    expect(getByPlaceholder('E-mail')).toBeTruthy();
+    expect(getByPlaceholder('Senha')).toBeTruthy();
+  });
+});
