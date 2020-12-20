a577d9336541304fb2314469e05c265adc230e33
Luan Castheloge <luanccp@gmail.com>
2020-12-20 14:18:27 +0000

    refactor: removing wrong comment

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   // we have to set the same IP as env in API
   // baseURL: 'http://192.168.15.17:3333',
+  // http://10.0.2.2:3333
   baseURL: 'http://192.168.1.8:3333',
 });
 
